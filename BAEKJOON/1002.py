1c90451f59032c8212f792aa2b7ff58747f8b2c7
HyeongMokJeong <jhm1062@naver.com>
2023-05-02 22:41:19 +0900

    ct

---
+++ BAEKJOON/1002.py
@@ -0,0 +1,11 @@
+import sys, math; input = sys.stdin.readline
+
+case = int(input().rstrip())
+for _ in range(case):
+    x1, y1, r1, x2, y2, r2 = map(int, input().split())
+    dis = math.sqrt((x1 - x2) ** 2 + (y1 - y2) ** 2)
+
+    if dis == 0 and r1 == r2: print(-1)
+    elif abs(r1 - r2) == dis or r1 + r2 == dis: print(1)
+    elif abs(r1 - r2) < dis < (r1 + r2): print(2)
+    else: print(0)
