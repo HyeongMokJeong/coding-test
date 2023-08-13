f72140ebea50e9a2bff34c82f20eb4b3998b2a54
HyeongMokJeong <jhm1062@naver.com>
2023-08-13 16:50:26 +0900

    ct

---
+++ BAEKJOON/4811.py
@@ -0,0 +1,12 @@
+import sys; input = sys.stdin.readline
+
+dp = [[0] * 31 for _ in range(31)]
+for i in range(1, 31): dp[0][i] = 1
+for i in range(1, 31):
+    for j in range(i, 31):
+        dp[i][j] += dp[i - 1][j] + dp[i][j - 1]
+
+while True:
+    n = int(input())
+    if not n: break
+    print(dp[n][n])
