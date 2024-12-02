4956d32954e0adcd0034d2bc9e5915cdf89b103e
HyeongMokJeong <jhm1062@naver.com>
2024-12-02 14:16:05 +0900

    24.12.02

---
+++ Programmers/배달.java
@@ -0,0 +1,48 @@
+import java.util.*;
+
+class Solution {
+    public int solution(int N, int[][] road, int K) {
+        int answer = 0;
+
+        List<int[]>[] graph = new ArrayList[N];
+        for (int i = 0; i < N; i++) graph[i] = new ArrayList<>();
+        for (int[] r : road) {
+            int a = r[0] - 1, b = r[1] - 1, c = r[2];
+
+            graph[a].add(new int[]{c, b});
+            graph[b].add(new int[]{c, a});
+        }
+        
+        int[] weight = new int[N];
+        Arrays.fill(weight, Integer.MAX_VALUE);
+        boolean[] visited = new boolean[N];
+        weight[0] = 0;
+        
+        Queue<int[]> q = new PriorityQueue<>(Comparator.comparingInt(a -> a[0]));
+        q.offer(new int[]{0, 0});
+        
+        while (!q.isEmpty()) {
+            int[] target = q.poll();
+            int idx = target[1], w = target[0];
+            
+            if (visited[idx]) continue;
+            visited[idx] = true;
+            
+            for (int[] next : graph[idx]) {
+                int nextIdx = next[1], nw = next[0];
+                int nextWeight = w + nw;
+
+                if (weight[nextIdx] > nextWeight) {
+                    weight[nextIdx] = nextWeight;
+                    q.offer(new int[]{nextWeight, nextIdx});
+                }
+            }
+        }
+
+        for (int w : weight) {
+            if (w <= K) answer += 1;
+        }
+
+        return answer;
+    }
+}
