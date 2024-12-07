05ac67b79292a837ebb5a215a0763f1ce0b5cbff
HyeongMokJeong <jhm1062@naver.com>
2024-12-07 19:25:11 +0900

    24.12.07

--- Programmers/표현 가능한 이진트리.java
+++ Programmers/표현 가능한 이진트리.java
@@ -1,39 +1,74 @@
+import java.util.*;
+
 class Solution {
-    public int[] solution(long[] numbers) {
-        int[] answer = new int[numbers.length];
+    private boolean[] visitedDist;
+    private int[] dist;
+    private List<int[]> allWeak = new ArrayList<>();
+    int answer = Integer.MAX_VALUE;
+    
+    public int solution(int n, int[] weak, int[] dist) {
+        visitedDist = new boolean[dist.length];
+        this.dist = dist;
         
-        for (int i = 0; i < numbers.length; i++) {
-            String binary = Long.toBinaryString(numbers[i]);
-            
-            // 타겟을 2진수로 변환하고 포화 이진트리를 만든다. (포화 이진트리의 노드 개수 = 2 ** h - 1)
-            // 2진수에서 오른쪽에 0을 추가하면 무시되므로, 왼쪽에만 붙인다.
-            int h = 0;
-            while((int) Math.pow(2, h) - 1 < binary.length()) h++;
-        	while((int) Math.pow(2, h) - 1 != binary.length()) binary = "0" + binary;
-        	
-            answer[i] = check(binary);
+        // 모든 weak 케이스를 만든다.
+        // n = 12, weak = [1, 5, 6, 10]인 경우
+        // [1, 5, 6, 10], [5, 6, 10, 13], [6, 10, 13, 17], [10, 13, 17, 18]
+        // 한쪽 방향으로 탐색해도 모든 경우를 탐색할 수 있다.
+        allWeak.add(weak.clone());
+        for (int i = 1; i < weak.length; i++) {
+            int temp = weak[0];
+            for (int j = 0; j < weak.length - 1; j++) {
+                weak[j] = weak[j + 1];
+            }
+            weak[weak.length - 1] = temp + n;
+            allWeak.add(weak.clone());
         }
-        return answer;
+        
+        // 모든 dist 케이스를 만든다.
+        for (int i = 0; i < dist.length; i++) {
+            int[] newDist = new int[dist.length];
+            newDist[0] = dist[i];
+            visitedDist[i] = true;
+            dfs(1, newDist);
+            visitedDist[i] = false;
+        }
+        
+        
+        return (answer == Integer.MAX_VALUE) ? -1 : answer;
     }
     
-    private int check(String target) {
-        int mid = (target.length() - 1) / 2;
-        char root = target.charAt(mid);
-        String left = target.substring(0, mid);
-        String right = target.substring(mid + 1, target.length());
-        
-        // 자식이 있는데 부모가 0이라면 표현할 수 없다.
-        if (root == '0' && 
-           (left.charAt((left.length() - 1) / 2) == '1' ||
-           right.charAt((right.length() - 1) / 2) == '1')
-           ) return 0;
-        
-        // 자식 트리가 리프 레벨이 아니라면 왼쪽, 오른쪽 재귀
-        if (left.length() >= 3) {
-            if (check(left) == 1 && check(right) == 1) return 1;
-            else return 0;
+    private void dfs(int idx, int[] newDist) {
+        if (idx == dist.length) {
+            // 완성된 각 weak 케이스에 대해, 모든 dist 케이스를 검사한다.
+            for (int[] w : allWeak) check(newDist, w);
+            return;
         }
-        // 리프 레벨이라면 항상 가능
-        return 1;
+        for (int i = 0; i < dist.length; i++) {
+            if (visitedDist[i]) continue;
+            
+            visitedDist[i] = true;
+            newDist[idx] = dist[i];
+            dfs(idx + 1, newDist);
+            visitedDist[i] = false;
+        }
+    }
+    
+    private void check(int[] newDist, int[] targetWeak) {
+        // cur = targetWeak idx
+        int cur = 0, next;
+        int distIdx = 0;
+        
+        while (cur < targetWeak.length && distIdx < newDist.length) {
+            next = cur + 1;
+            // targetWeak 범위 내이면서,
+            // 현재 취약 좌표 + 현재 처리길이가 다음 취약점보다 크다면(커버 가능하다면) 다음 취약점 좌표 탐색
+            while (next < targetWeak.length && 
+                   targetWeak[cur] + newDist[distIdx] >= targetWeak[next]) next += 1;
+            
+            cur = next;
+            distIdx += 1;
+        }
+        // 모든 취약점을 확인했다면 최솟값 갱신
+        if (cur == targetWeak.length && distIdx < answer) answer = distIdx;
     }
 }
